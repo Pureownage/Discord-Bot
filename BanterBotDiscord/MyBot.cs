e7d7228f650e4a556d1b7136b4bf3e6fb2102721
Jake <jwillamson@hotmail.co.uk>
2017-01-07 02:02:17 +0000

    First Commit of program with code
    This contains the basic code for the discord bot I have created. Removed
    the string needed to connect to the server, so running the code will
    fail to connect.

---
+++ BanterBotDiscord/MyBot.cs
@@ -0,0 +1,78 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanterBotDiscord
+{
+    class MyBot
+    {
+        DiscordClient Discord;
+        CommandService Commands;
+        Random Rand;
+
+        string[,] Users;
+        string CheckEm = "Images/CheckEm.jpg";
+        string[] RandomImage; 
+
+        public MyBot()
+        {
+            Rand = new Random();
+            RandomImage = new string[]
+            {
+                "Images/Image1.jpg",
+                "Images/Image2.jpg",
+                "Images/Image3.jpg"
+            };
+            //Users = new string[,];
+
+            Discord = new DiscordClient(X =>
+            {
+                X.LogLevel = LogSeverity.Info;
+                X.LogHandler = Log;
+            });
+            Discord.UsingCommands(X =>
+            {
+                X.PrefixChar = '!';
+                X.AllowMentionPrefix = true;
+
+            });
+            
+
+            Commands = Discord.GetService<CommandService>();
+
+            Commands.CreateCommand("CanYouHearMe?").Do(async (e) => { e.Channel.SendMessage("Hell Yes");});
+            Commands.CreateCommand("Double Number?").Do(async (e) => {
+                int RandomDubs = Rand.Next(10,100);
+                string RandomDubsString = RandomDubs.ToString();
+                e.Channel.SendMessage(RandomDubsString);
+                e.Channel.SendFile(CheckEm);
+            });
+            RegisterCommand();
+
+            Discord.ExecuteAndWait(async () =>
+            {
+                await Discord.Connect("", TokenType.Bot);
+            });
+        }
+
+        public void RegisterCommand()
+        {
+            Commands.CreateCommand("TestCommand").Do(async (e) =>
+            {
+                int RandomNumber = Rand.Next(RandomImage.Length);
+                string ImageToPost = RandomImage[RandomNumber];
+                await e.Channel.SendFile(ImageToPost);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ BanterBotDiscord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanterBotDiscord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+           // string Text = System.IO.File.ReadAllText("C:/Users/Pure/Documents/visual studio 2015/Projects/BanterBotDiscord/BanterBotDiscord/Files/Users.txt");
+
+            //Console.WriteLine("Contents of textfile = {0}", Text);
+        }
+    }
+}
